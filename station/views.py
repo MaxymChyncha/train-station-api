46f909987a031c73dd965dc40ed6a1ffdd83917f
MaxymChyncha <max.chyncha@gmail.com>
2024-03-23 16:12:18 +0200

    feat(station): create basic serializer & view & router for train_type model

--- station/views.py
+++ station/views.py
@@ -1,7 +1,8 @@
 from rest_framework import mixins, viewsets
 
-from station.models import Crew
+from station.models import Crew, TrainType
 from station.serializers.crew_serializers import CrewSerializer
+from station.serializers.train_type_serializers import TrainTypeSerializer
 
 
 class CrewViewSet(
@@ -11,3 +12,12 @@ class CrewViewSet(
 ):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+
+
+class TrainTypeViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
