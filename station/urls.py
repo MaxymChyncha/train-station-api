b8581e16d8f9eea3b81f713da84be9c6c9f8529f
MaxymChyncha <max.chyncha@gmail.com>
2024-03-23 15:57:38 +0200

    feat(station): create basic serializer & view & router for crew model

--- station/urls.py
+++ station/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from station.views import CrewViewSet
+
+router = routers.DefaultRouter()
+router.register("crews", CrewViewSet, basename="crew")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
 
 app_name = "station"
--- station/views.py
+++ station/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from station.models import Crew
+from station.serializers.crew_serializers import CrewSerializer
+
+
+class CrewViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
