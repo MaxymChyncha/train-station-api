b2ca0ed760cd04ea2074535f1756afb4590b17dc
MaxymChyncha <max.chyncha@gmail.com>
2024-03-23 16:12:18 +0200

    feat(station): create basic serializer & view & router for train_type model

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from station.views import CrewViewSet
+from station.views import CrewViewSet, TrainTypeViewSet
 
 router = routers.DefaultRouter()
 router.register("crews", CrewViewSet, basename="crew")
+router.register("train_types", TrainTypeViewSet, basename="train-type")
 
 urlpatterns = [
     path("", include(router.urls))
