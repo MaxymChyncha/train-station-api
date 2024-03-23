f11eb4d2770321f9b51b85631546eee6eb36c5f7
MaxymChyncha <max.chyncha@gmail.com>
2024-03-23 15:21:34 +0200

    feat(station): register station models to admin panel

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from station.models import (
+    Crew,
+    TrainType,
+    Train,
+    Station,
+    Route,
+    Trip,
+    Order,
+    Ticket
+)
+
+admin.site.register(Crew)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Trip)
+admin.site.register(Order)
+admin.site.register(Ticket)
