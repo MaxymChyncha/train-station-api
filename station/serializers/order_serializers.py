3dc1781762c63d985eeb0dba34a72a2bc9d2b61c
MaxymChyncha <max.chyncha@gmail.com>
2024-03-24 11:21:24 +0200

    feat(station): add feature for creating tickets in order serializer

--- station/serializers/order_serializers.py
+++ station/serializers/order_serializers.py
@@ -1,13 +1,21 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from station.models import Order
+from station.models import Order, Ticket
+from station.serializers.ticket_serializers import TicketSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, allow_empty=False)
 
     class Meta:
         model = Order
-        fields = (
-            "id",
-            "created_at",
-        )
+        fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
