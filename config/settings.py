9cbaa4072154bde6d9bc22c5ac5f639cd1db2b64
MaxymChyncha <max.chyncha@gmail.com>
2024-03-29 14:38:48 +0200

    feat(config): add DEFAULT_PERMISSION_CLASSES in settings.py

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "station.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ),
     "DEFAULT_PAGINATION_CLASS": (
         "rest_framework.pagination.PageNumberPagination"
     ),
